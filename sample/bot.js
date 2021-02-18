29bec30dac369afa4bdf51a2d576fafdf1bb8413
mk788 <mk788@kent.ac.uk>
2021-02-18 22:10:27 +0000

    "(fix#2)"

--- sample/bot.js
+++ sample/bot.js
@@ -2,6 +2,8 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+  if (query.indexOf("tea") != -1 || query.indexOf("biscuit") != -1)//adding tea and biscuit bit-mk788
+    return "I do not care, I too like to have tea with biscuits, instead, I'm locked inside a data center.";  
   return "Sorry Dave, I cannot do that."
 }
 
